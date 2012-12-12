3e3f192c8255b8460875af4b5b074c521e30d871
czs_chen81@msn.com <czs_chen81@msn.com@ef262f9b-5694-5084-4050-eb58d5921e16>
2012-12-12 14:23:49 +0000

    This is a test

--- trunk/src/QuinceCommon/QuinceCore/Utility/File.cpp
+++ trunk/src/QuinceCommon/QuinceCore/Utility/File.cpp
@@ -8,9 +8,32 @@
 
 #include "Stdafx.h"
 #include "File.h"
+#include <sys/stat.h>
 
 using namespace std;
 using namespace QuinceSystem;
+
+namespace
+{
+
+#ifdef PATH_MAX
+const size_t kMaxPathLength = PATH_MAX;
+#else
+const size_t kMaxPathLength = 4096;
+#endif
+
+string GetAbsolutePath(const string& path)    
+{
+    char absolutePath[kMaxPathLength];
+    if(!realpath(path.c_str(), absolutePath))
+    {
+        // message error
+        return "";
+    }
+    return absolutePath;
+}
+
+} // anonymous namespace
 
 File::File()
 	: mFilePath(""), 
@@ -42,3 +65,82 @@ File::File(const File& file)
 File::~File()
 {
 }
+
+string
+File::filePath(bool absolute) const
+{
+    return absolute ? ::GetAbsolutePath(mFilePath) : mFilePath;
+}
+
+string 
+File::pathName(bool absolute) const
+{
+    return absolute ? ::GetAbsolutePath(mPathName) : mPathName;
+}
+
+string 
+File::fileName() const
+{
+    return mFileName;
+}
+
+string
+File::baseName() const
+{
+    return mBaseName;
+}
+
+string
+File::extension(bool complete) const
+{
+    if(complete)
+    {
+        return mExtension;
+    }
+    else
+    {
+        int last_dot_pos = mExtension.find_last_of('.');
+        return mExtension.substr(last_dot_pos + 1);
+    }
+}
+
+bool
+File::parse(const string& file_path)
+{
+    mFilePath = file_path;
+    int last_sep_pos = file_path.find_last_of(File::Separator());
+    if(last_sep_pos < 0)
+    {
+        mPathName = ".";
+        mFileName = file_path;
+    }
+    else if(last_sep_pos == 0)
+    {
+        mPathName = File::Separator();
+        mFileName = file_path.substr(last_sep_pos + 1);
+    }
+    else
+    {
+        mPathName = string(file_path.begin(), file_path.begin() + last_sep_pos);
+        mFileName = file_path.substr(last_sep_pos + 1);
+    }
+    
+    int first_dot_pos = mFileName.find_first_of('.');
+    if(first_dot_pos < 0)
+    {
+        mBaseName = mFileName;
+        mExtension = "";
+    }
+    else
+    {
+        mBaseName = string(mFileName.begin(), mFileName.begin() + first_dot_pos);
+        mExtension = string(mFileName.begin() + first_dot_pos + 1, mFileName.end());
+    }
+    return true;
+}
+
+string
+File::Separator()
+{
+    return "/";
+} 
--- trunk/src/QuinceCommon/QuinceCore/Utility/File.h
+++ trunk/src/QuinceCommon/QuinceCore/Utility/File.h
@@ -24,12 +24,27 @@ namespace QuinceSystem
 		File(const std::string& file_path);
 		File(const File& file);
 		virtual ~File();
-	protected:
-		std::string mFilePath;
-		std::string mPathName;
-		std::string mFileName;
-		std::string mBaseName;
-		std::string mExtension;
+
+        public:
+            string filePath(bool absolute = false) const;
+            string pathName(bool absolute = false) const;
+            string fileName() const;
+            string baseName() const;
+            string extension(bool complete = false) const;
+
+            //size_t byteSize() const;
+            //bool isFile() const;
+            //bool exists() const;
+            bool parse(const string& file_path);
+
+        public:
+            static string Separator();
+	private:
+		string mFilePath;
+		string mPathName;
+		string mFileName;
+		string mBaseName;
+		string mExtension;
 	};
 }
 
